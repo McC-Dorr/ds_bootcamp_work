b3365bfc30b20fb0f326a2f33fa0784ded95b704
McC-Dorr <164630410+McC-Dorr@users.noreply.github.com>
2024-03-31 08:49:07 +0100

    Add files via upload

---
+++ logic.py
@@ -0,0 +1,24 @@
+# User wants a clothes rec based on weather temperature
+# Define function for what clothes they should select based on temp
+# Capture user temperature input
+# Print recommended clothes
+
+# Function that has flawed logic because of the order of elifs
+def choose_clothes(temp):
+    if temp > 80:
+        return 'Wear a t-shirt and hula skirt.'
+    elif temp > 30:
+        return 'Wear a coat over many layers and thick socks.'
+    elif temp > 60:
+        return 'Wear long sleeves and shorts.'
+    else:
+        return "Don't go out!"  # Used double quotes because of the ' in don't
+
+# Capture user input
+current_temp = float(input("What is the temperature outside today:"))
+
+# Use temperature to tell the user what to wear
+clothing_suggestion = choose_clothes(current_temp)
+
+# Tell them
+print(clothing_suggestion)
